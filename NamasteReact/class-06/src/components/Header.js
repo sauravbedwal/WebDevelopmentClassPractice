683d4906681e63e4fb16da399bef0a50d39c3c69
sauravbedwal <sauravbedwal1234@gmail.com>
2024-04-20 18:58:27 +0530

    Namaste React class 6

---
+++ NamasteReact/class-06/src/components/Header.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+
+const Header = () => {
+
+    const [btnNameReact, setBtnNameReact] = useState("Login");
+
+    return (
+        <div className="header">
+            <div className="logo-container">
+                {/* <img className="logo" src="https://img.lovepik.com/element/45006/2391.png_860.png" /> */}
+                <img className="logo" src={LOGO_URL} />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <button className="login" onClick={() => {
+                        btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login");
+                    }}>{btnNameReact}</button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
