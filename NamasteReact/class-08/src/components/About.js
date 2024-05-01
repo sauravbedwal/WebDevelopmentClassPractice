ab19ea798c490c41c20e419acb2231965344024c
sauravbedwal <sauravbedwal1234@gmail.com>
2024-05-01 19:47:19 +0530

    Class 8

---
+++ NamasteReact/class-08/src/components/About.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import User from './User';
+import UserClass from './UserClass';
+
+class About extends React.Component {
+    constructor(props) {
+        super(props)
+
+        // console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        // console.log("Parent Component Did Mount");
+    }
+
+    render() {
+        // console.log("Parent render");
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is Namaste React Web Series</h2>
+                <UserClass name={"First (class)"} location={"Delhi Class"} />
+                {/* <UserClass name={"Second (class)"} location={"US"} /> */}
+                {/* <User /> */}
+            </div>
+        );
+    }
+}
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About</h1>
+//             <h2>This is Namaste React Web Series</h2>
+//             <UserClass name={"Saurav Bedwal (class)"} location={"Delhi Class"} />
+//         </div>
+//     );
+// };
+
+export default About;
+
+/**
+   - Parent constructor
+   - Parent render
+   
+      - First constructor
+      - First render
+
+      - Second constructor
+      - Second render
+
+    <DOM UPDATED - IN SINGLE BATCH>
+
+      - First componentDidMount
+      - Second componentDidMount
+
+   -Parent componentDidMount
+
+ */
