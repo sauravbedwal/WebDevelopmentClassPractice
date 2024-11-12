4e771d09ff7f09738956ea84cd6cb8aa8361aee0
Saurav Bedwal <sauravbedwal1234@gmail.com>
2024-11-13 03:42:24 +0530

    commit all api's

--- NamasteNode/Season-02/class-07/src/app.js
+++ NamasteNode/Season-02/class-07/src/app.js
@@ -21,6 +21,118 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// // GET user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailID;
+  console.log(userEmail);
+
+  try {
+    // const userEmail = req.body.emailId;
+    // console.log(userEmail);
+
+    const users = await User.find({ emailId: userEmail });
+
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// GET user by email but only One User
+// app.get("/user", async (req, res) => {
+//   try {
+//     const userEmail = req.body.emailId;
+//     console.log(userEmail);
+//     const user = await User.findOne({ emailId: userEmail });
+
+//     if (!user) {
+//       res.status(404).send("user not found");
+//     } else {
+//       res.send(user);
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something went wrong");
+//   }
+// });
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// DELETE API - DELETE /user - delete the user
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body._id;
+    console.log(userId);
+
+    const user = await User.findByIdAndDelete(userId);
+
+    // we can write like this as its normal way and above is the shorthand for this
+    // const user = await User.findByIdAndDelete({_id: userId});
+
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User deleted successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//// PATCH API - Update data of the user
+// app.patch("/user", async (req, res) => {
+//   try {
+//     const userId = req.body._id;
+//     const data = req.body;
+
+//     const user = await User.findByIdAndUpdate(userId, data);
+//     // const user = await User.findByIdAndUpdate(userId, { firstName: "Ishant" });
+//     if (!user) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.send("User updated successfully");
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something went wrong");
+//   }
+// });
+
+// PATCH API - Update data of the user but also added Optional 3rd parameter
+app.patch("/user", async (req, res) => {
+  try {
+    const userId = req.body._id;
+    const data = req.body;
+
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "before",
+    });
+    console.log(user);
+
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send("User updated successfully");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection estabilished..");
